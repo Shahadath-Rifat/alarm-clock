b269aaba4268ae7d73668d8b7428885d6d11a8e8
Rifat <rifatsh27@gmail.com>
2023-03-10 11:53:52 -0600

    completeting CSS

--- assests/script/index.js
+++ assests/script/index.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const clock = document.getElementById('clock');
+
+
+// Update clock every second
+setInterval(() => {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    clock.textContent = timeString;
+}, 1000);
+
